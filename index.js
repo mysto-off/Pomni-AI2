ca5aefa0378388f5b7c468fd4a76ecabf564b501
mysto off <khadabfest@gmail.com>
2026-08-28 20:45:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '212627546849', // Bot number
+  phoneNumber: '212718891104', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [

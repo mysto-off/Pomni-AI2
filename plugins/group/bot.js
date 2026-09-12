bb74080f9967c5b68c7706ea6a35ce3e97161a4b
mysto off <khadabfest@gmail.com>
2026-09-12 12:18:33 +0100

    Delete plugins/group directory

--- plugins/group/bot.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    await conn.circular(m.chat, { vid: "https://c.termai.cc/v131/4gX.mp4", sec: 200 }, m);
-}
-
-handler.command = ["بوم"];
-handler.usePrefix = false;
-
-export default handler;
--- plugins/group/mylink.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-m.reply(`wa.me/${m.sender.split("@")[0]}`)
-}
-handler.usage = ["رابطي"];
-handler.category = "group";
-handler.command = ["رابطي"];
-
-export default handler;
